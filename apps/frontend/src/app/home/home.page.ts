8965eaacfaf2f90dda67d7cfe1c725e45c863c13
Kaktusryak <denis1243qwerty@gmail.com>
2024-05-15 14:55:45 +0300

    start

---
+++ apps/frontend/src/app/home/home.page.ts
@@ -0,0 +1,12 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonInput } from '@ionic/angular';
+
+@Component({
+  selector: 'organization-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  constructor() {}
+
+}
